75a47895bd04ac109023aed38a38b599ac27f141
PmasCastro <pmas.castro@gmail.com>
2025-04-08 02:06:38 +0100

    added a basic html boilerplate

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ import ollama
 from dotenv import load_dotenv
 from openai import OpenAI
 
-load_dotenv('../.env')
+# load_dotenv('../.env')
+load_dotenv()
+openai_api_key = os.getenv('OPENAI_API_KEY')
+
+llama_model= 'llama3.2'
+
--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from phil_bots import views  # Import views from your app
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),  # Home view at the root URL
 ]
